f282dfc2baa14287a0d5179aa01636767b2c6113
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-03-25 18:14:53 +0100

    [DONE] the end

--- script.js
+++ script.js
@@ -5,15 +5,22 @@ function cambiaImmagine() {
         immagine.src = './img/white_lamp.png';
         immagine.classList.remove('regolazione'); // rimuove la classe
     } else {
-        immagine.src = './img/yellow_lamp.png'; 
+        immagine.src = './img/yellow_lamp.png';
         immagine.classList.add('regolazione'); //aggiunge la classe
     }
-   
-}
-bottone.addEventListener('click', cambiaImmagine); 
 
-onclick = bottone.click('color')
- function cambiaColore() {
+}
+bottone.addEventListener('click', cambiaImmagine);
+
+function cambiaColore() {
     console.log('cambia colore');
-    immagine.classList.add('colore');
- }
+    bottone.classList.add('colore');
+}
+function cambiaColore1() {
+    bottone.classList.remove('colore');
+    
+}
+
+bottone.addEventListener('mouseup', cambiaColore);
+bottone.addEventListener('mousedown', cambiaColore1);
+
