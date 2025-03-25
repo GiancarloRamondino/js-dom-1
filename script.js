6035068ae334ecc70b89a7a2d22c25cbd4da4c60
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-03-25 17:39:54 +0100

    [UPD]  lamp

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ let bottone = document.getElementById('btn');
 function cambiaImmagine() {
     if (immagine.src.includes('img/yellow_lamp.png')) {
         immagine.src = './img/white_lamp.png';
+        immagine.classList.remove('regolazione'); // rimuove la classe
     } else {
-        immagine.src = './img/yellow_lamp.png';
+        immagine.src = './img/yellow_lamp.png'; 
+        immagine.classList.add('regolazione'); //aggiunge la classe
     }
+   
 }
-bottone.addEventListener('click', cambiaImmagine);
+bottone.addEventListener('click', cambiaImmagine); 
+
+onclick = bottone.click('color')
+ function cambiaColore() {
+    console.log('cambia colore');
+    immagine.classList.add('colore');
+ }
